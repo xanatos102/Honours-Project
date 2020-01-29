f82e41fd4e330b637447328a2c6e9e24873ac37a
aaronhay92 <1800855@uad.ac.uk>
2020-01-29 12:17:51 +0000

    Renamed topic-form.php to question-form.php. Renamed some variables.

--- View/question-form.php
+++ View/question-form.php
@@ -2,46 +2,45 @@
 include 'session.php';
 include 'header.php';
 ?>
-
 <!DOCTYPE html>
 <html>
 <body>
 
 <div class="container">
 
-  <h1 class="text-center mt-4">Create Topic</h1>
-  <form class="form-group needs-validation" action="../Controller/attempt-create-topic.php" method="POST" novalidate>
+  <h1 class="text-center mt-4">Add Question</h1>
+  <form class="form-group needs-validation" action="../Controller/attempt-create-question.php" method="POST" novalidate>
     <div class="form-group">
-      <label for="exampleDescription">Topic Description (Required)</label>
-      <textarea class="form-control" id="description" name="description" placeholder="Topic Description" rows="3" required></textarea>
+      <label for="questionDescription">Question Description (Required)</label>
+      <textarea class="form-control" id="questionDescription" name="questionDescription" placeholder="Question Description" rows="3" required></textarea>
       <div class="invalid-feedback">
         You cannot Leave This field Empty.
       </div>
     </div>
     <div class="form-group">
-      <label for="exampleAnswerOne">Answer One (Required)</label>
+      <label for="answerOne">Answer One (Required)</label>
       <input type="text" class="form-control" id="answerOne" name="answerOne" placeholder="Answer One" required>
       <div class="invalid-feedback">
         You cannot Leave This field Empty.
       </div>
     </div>
     <div class="form-group">
-      <label for="exampleAnswerTwo">Answer Two (Required)</label>
+      <label for="answerTwo">Answer Two (Required)</label>
       <input type="text" class="form-control" id="answerTwo" name="answerTwo" placeholder="Answer Two" required>
       <div class="invalid-feedback">
         You cannot Leave This field Empty.
       </div>
     </div>
     <div class="form-group">
-      <label for="exampleAnswerThree">Answer Three (Optional)</label>
+      <label for="answerThree">Answer Three (Optional)</label>
       <input type="text" class="form-control" id="answerThree" name="answerThree" placeholder="Answer Three">
     </div>
     <div class="form-group">
-      <label for="exampleAnswerFour">Answer Four (Optional)</label>
+      <label for="answerFour">Answer Four (Optional)</label>
       <input type="text" class="form-control" id="answerFour" name="answerFour" placeholder="Answer Four">
     </div>
     <div class="form-group">
-      <label for="exampleCorrectAnswer">Correct Answer (Required)</label>
+      <label for="correctAnswer">Correct Answer (Required)</label>
       <select class="form-control" id="correctAnswer" name="correctAnswer" required>
         <option selected>Choose...</option>
         <option>Answer One</option>
@@ -54,13 +53,13 @@ include 'header.php';
       </select>
     </div>
     <div class="form-group">
-      <label for="exampleCategory">Category (Required)</label>
+      <label for="category">Category (Required)</label>
       <input type="text" class="form-control" id="category" name="category" placeholder="Category (e.g. Malware, Phishing.)" required>
       <div class="invalid-feedback">
         You cannot Leave This field Empty.
       </div>
     </div>
-    <button type="submit" id="createTopic" name="createTopic" class="btn btn-secondary">Submit</button>
+    <button type="submit" id="submitQuestion" name="submitQuestion" class="btn btn-secondary">Submit</button>
   </form>
 
 </div>
