8fb42f373025aecf658e75970ed6ada436a191fd
Aaron Hay <43172236+GorillaZombie92@users.noreply.github.com>
2020-03-24 17:10:01 +0000

    Added Abertay logo to footer. Need to update content before testing.

--- View/footer.php
+++ View/footer.php
@@ -20,7 +20,7 @@
           <h4 class="text-uppercase">Business Information</h4>
           <p class="lead"> Author: Aaron Hay </p>
           <p class="lead"> Student ID: 1800855 </p>
-          <p class="lead"> Address: Abertay University, Bell Street, Dundee, Scotland, DD1 1HG </p>
+          <p class="lead" style="align-items: center; "><img src="images/abertay.png" alt="professor" style="width: 1.2em; height: 1.2em; margin-right: 0.25em;"/>Abertay University, Bell Street, Dundee, Scotland, DD1 1HG</p>
           <p>
             <!-- <a href="http://www.facebook.com/share.php?u=https://mayar.abertay.ac.uk/~1403163/DPH/View/index.php" class="fab fa-facebook-square fa-3x"></a>
             <a href="https://twitter.com/intent/tweet?url=https://mayar.abertay.ac.uk/~1403163/DPH/View/index.php" class="fab fa-twitter-square fa-3x"></a>
